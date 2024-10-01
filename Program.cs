21beaaa141eabd614cc756c0e5ab07d42fb3501d
Guilherme Mendes <guilheu2004@gmail.com>
2024-10-01 15:05:40 -0300

    primeiro desafio

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacciEstagio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> fibonacci = new List <int> ();
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            int numero;
+            Console.WriteLine("Informe o número a ser checado");
+            bool ehNumero = int.TryParse(Console.ReadLine(), out int resultado);
+            numero = resultado;
+
+            while (!ehNumero)
+            {
+
+                if (ehNumero)
+                {
+                    numero = resultado;
+                }
+                else
+                {
+                    Console.WriteLine("Deve ser um inteiro válido!");
+                    ehNumero = int.TryParse(Console.ReadLine(), out int resultadoDnv);
+                    numero = resultadoDnv;
+
+                }
+            }
+            
+
+            for (int ultimo = 0; ultimo <= numero; ultimo = a) 
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                
+                fibonacci.Add(c);
+                
+            }
+            if (fibonacci.Contains(numero))
+            {
+                Console.WriteLine($"O numero {numero} pertence a sequencia");
+            }
+            else
+            {
+                Console.WriteLine($"O numero {numero} não pertence a sequencia");
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("fibonacciEstagio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fibonacciEstagio")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("ca31d3ce-26bb-46e2-9f36-e3c2fdfcb500")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
